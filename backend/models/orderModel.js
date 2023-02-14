0526fcb9c923963aa804bcfc2730b948c5ab75f2
alicarpio <agcarpio@espol.edu.ec>
2023-02-14 08:43:42 -0500

    connected the mongo db

---
+++ backend/models/orderModel.js
@@ -0,0 +1,73 @@
+import mongoose from "mongoose";
+
+const orderSchema = mongoose.Schema({
+    name: {type: String, required: true},
+    rating: {type: String, required: true},
+    comment: {type: String, required: true}
+}, {
+    timestamps: true
+})
+const productSchema = mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User'
+        },
+        orderItems:[
+            {
+                name: {type: String, required: true}
+                name: {type: String, required: true}
+                name: {type: String, required: true}
+                name: {type: String, required: true}
+            }
+        ],
+        brand: {
+            type: String,
+            required: true,
+
+        },
+        category: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        reviews: [reviewSchema],
+        rating: {
+            type: Number,
+            required: true,
+            default: 0
+        },
+        numReviews: {
+            type: Number,
+            required: true,
+            default: 0
+        },
+        price: {
+            type: Number,
+            required: true,
+            default: 0
+        },
+        countInStock: {
+            type: Number,
+            required: true,
+            default: 0
+        },
+
+
+        isAdmin: {
+            type: boolean,
+            required: true,
+            default: false
+        }
+
+    }, {
+        timestamps: true
+    })
+
+const Product = mongoose.model('User', userSchema)
+
+export default User
