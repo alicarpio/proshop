91e208815893697d0bcd217bf22aac2345ffaaa3
alicarpio <agcarpio@espol.edu.ec>
2023-04-05 21:44:52 -0500

    add .env

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,23 @@ dotenv.config()
 connectDB()
 
 const app = express()
+
+
 const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => {
     res.send('API is running...');
 })
 
 app.use('/api/products', productsRoutes)
+
+app.use((err,req,res,next)=>{
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+    res.status(statusCode)
+    res.json({
+        message: err.message,
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+    })
+})
 
 
 app.listen(
